308ccd662cfa08f5ed5a60fb8b1f66c475e52f20
Clinton Dmello <clinton.dmello95@outlook.com>
2020-08-22 23:00:32 +0530

    Added users app to handle user registration also got rid of hardcoded post details.

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [{
-            'author' :'Clinton Dmello',
-            'title':'Blog Post 1',
-            'content':'First post content',
-            'date_posted':'January 25,2020'
-      },
-      {
-            'author' :'Swebert Dmello',
-            'title':'Blog Post 2',
-            'content':'Second post content',
-            'date_posted':'January 26,2020'
-      },]
+#Sample Post Data
+#posts = [
+#        {
+#            'author' :'Clinton Dmello',
+#            'title':'Blog Post 1',
+#            'content':'First post content',
+#            'date_posted':'January 25,2020'
+#      },
+#      {
+#            'author' :'Swebert Dmello',
+#            'title':'Blog Post 2',
+#            'content':'Second post content',
+#            'date_posted':'January 26,2020'
+#      }
+#      ]
 
 def home(request):
     context = {
-        'posts':posts
+        'posts':Post.objects.all()
         }
     return render(request,'blog/home.html',context) #Should return HttpResponse or an Expection
 
---
+++ users/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User #form.save will save to the User Model
+        fields = ['username','email','password1','password2']
